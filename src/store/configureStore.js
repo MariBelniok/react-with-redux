538f0f6bbfc640fbae5bec59084dd51686b3446b
maribelniok <mariannabelniok@outlook.com>
2022-06-07 19:21:26 -0300

    created generic async request

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,10 @@
 import { configureStore, combineReducers, getDefaultMiddleware } from '@reduxjs/toolkit';
 import contador from './contador/contador.js';
 import modal from './modal/modal.js'
-import logger from './middleware/logger.js'
+import login from './login/login.js';
 
-const reducer = combineReducers({ contador, modal });
-const middleware = [...getDefaultMiddleware(), logger]
+const reducer = combineReducers({ contador, modal, login });
+const middleware = [...getDefaultMiddleware()]
 const store = configureStore({ reducer, middleware });
 
 export default store;
